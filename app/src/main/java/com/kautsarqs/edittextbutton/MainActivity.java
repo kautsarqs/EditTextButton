63b9c689a5b2355dae9f2b61c5e9557e24977f1f
kautsarqs <kaustar1777@gmail.com>
2025-03-14 13:54:08 +0700

    Initial commit

---
+++ app/src/main/java/com/kautsarqs/edittextbutton/MainActivity.java
@@ -0,0 +1,35 @@
+package com.kautsarqs.edittextbutton;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Button okButton = findViewById(R.id.okButton);
+        TextView textview = findViewById(R.id.infoTextView);
+        okButton.setOnClickListener(v -> {
+            String nama = ((EditText) findViewById(R.id.namaEditText)).getText().toString();
+            textview.setText("Nama anda adalah " + nama);
+        });
+
+    }
+}
